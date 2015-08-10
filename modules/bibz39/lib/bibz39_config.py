32769e5546a425a68f32875f11f8948555e4c86b
Guillaume Lastecoueres <PX9e@gmx.fr>
2015-08-10 20:02:36 +0200

    Fix z39 server configuration

--- modules/bibz39/lib/bibz39_config.py
+++ modules/bibz39/lib/bibz39_config.py
@@ -4,9 +4,6 @@
 CFG_Z39_SERVER = {
     'Library of Congress': {'address': 'lx2.loc.gov', 'port': 210, 'databasename': 'LCDB',
                             'preferredRecordSyntax': 'USMARC'},
-    'Library of Congress MARC8': {'address': 'lx2.loc.gov', 'port': 210,
-                                  'databasename': 'LCDB_MARC8',
-                                  'preferredRecordSyntax': 'USMARC', 'default': True},
     'UNOG Library': {'address': 'eu.alma.exlibrisgroup.com', 'port': 1921,
                      'databasename': '41UNOG_INST',
                      'preferredRecordSyntax': 'USMARC', 'default': True},

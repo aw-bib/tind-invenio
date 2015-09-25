4cfb4d09d016d2a10ad71834a820bfb7f08b75f0
Guillaume Lastecoueres <PX9e@gmx.fr>
2015-09-25 17:08:48 +0200

    hotfixes

--- modules/bibcirculation/lib/bibcirculation_templates.py
+++ modules/bibcirculation/lib/bibcirculation_templates.py
@@ -1241,9 +1241,6 @@ class Template:
                  <td>%s</td>
                  <td>%s</td>
                  <td>%s</td>
-                 <td>%s</td>
-                 <td>%s</td>
-                 <td>%s</td>
                  <td algin='center'>
                  """ % (borrower_link,
                        title_link,

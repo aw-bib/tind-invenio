e335049e2fc830af260f870fe99491c58eb11720
Guillaume Lastecoueres <PX9e@gmx.fr>
2015-10-05 21:54:31 +0200

    New bibindex type + new tokenizer

--- modules/bibindex/lib/bibindex_engine_config.py
+++ modules/bibindex/lib/bibindex_engine_config.py
@@ -31,26 +31,26 @@ from invenio.config import CFG_VERSION, CFG_PYLIBDIR
 BIBINDEX_ENGINE_VERSION = "Invenio/%s bibindex/%s" % (CFG_VERSION, CFG_VERSION)
 
 # safety parameters concerning DB thread-multiplication problem:
-CFG_CHECK_MYSQL_THREADS = 0 # to check or not to check the problem?
-CFG_MAX_MYSQL_THREADS = 50 # how many threads (connections) we
-                           # consider as still safe
-CFG_MYSQL_THREAD_TIMEOUT = 20 # we'll kill threads that were sleeping
-                              # for more than X seconds
+CFG_CHECK_MYSQL_THREADS = 0  # to check or not to check the problem?
+CFG_MAX_MYSQL_THREADS = 50  # how many threads (connections) we
+# consider as still safe
+CFG_MYSQL_THREAD_TIMEOUT = 20  # we'll kill threads that were sleeping
+# for more than X seconds
 
-CFG_BIBINDEX_SYNONYM_MATCH_TYPE = { 'None': '-None-',
-                                    'exact': 'exact',
-                                    'leading_to_comma': 'leading_to_comma',
-                                    'leading_to_number': 'leading_to_number'}
+CFG_BIBINDEX_SYNONYM_MATCH_TYPE = {'None': '-None-',
+                                   'exact': 'exact',
+                                   'leading_to_comma': 'leading_to_comma',
+                                   'leading_to_number': 'leading_to_number'}
 
 CFG_BIBINDEX_COLUMN_VALUE_SEPARATOR = ","
 
-CFG_BIBINDEX_INDEX_TABLE_TYPE = { 'Words': 'WORD',
-                                  'Pairs': 'PAIR',
-                                  'Phrases': 'PHRASE' }
+CFG_BIBINDEX_INDEX_TABLE_TYPE = {'Words': 'WORD',
+                                 'Pairs': 'PAIR',
+                                 'Phrases': 'PHRASE'}
 
-CFG_BIBINDEX_WASH_INDEX_TERMS = { 'Words': 50,
-                                  'Pairs': 100,
-                                  'Phrases': 0}
+CFG_BIBINDEX_WASH_INDEX_TERMS = {'Words': 50,
+                                 'Pairs': 100,
+                                 'Phrases': 0}
 
 CFG_BIBINDEX_TOKENIZERS_PATH = os.path.join(CFG_PYLIBDIR, 'invenio', 'bibindex_tokenizers')
 
@@ -58,16 +58,17 @@ CFG_BIBINDEX_ADDING_RECORDS_STARTED_STR = "%s adding records #%d-#%d started"
 
 CFG_BIBINDEX_UPDATE_MESSAGE = "Searching for records which should be reindexed..."
 
-CFG_BIBINDEX_UPDATE_MODE = { 'Update': 'update',
-							 'Insert': 'insert',
-							 'Remove': 'remove' }
+CFG_BIBINDEX_UPDATE_MODE = {'Update': 'update',
+                            'Insert': 'insert',
+                            'Remove': 'remove'}
 
 CFG_BIBINDEX_TOKENIZER_TYPE = {"string": "string",
                                "multifield": "multifield",
                                "recjson": "recjson",
+                               "full": "full",
                                "unknown": "unknown"}
 
 CFG_BIBINDEX_SPECIAL_TAGS = {'8564_u': {'Words': 'BibIndexFulltextTokenizer',
                                         'Pairs': 'BibIndexEmptyTokenizer',
                                         'Phrases': 'BibIndexEmptyTokenizer'}
-                            }
+                             }
